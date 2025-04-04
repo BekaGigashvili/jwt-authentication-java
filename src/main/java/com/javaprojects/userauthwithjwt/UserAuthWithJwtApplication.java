227930a1712ce984681e33a5500c61d65094134b
Beka Gigashvili <bekagigashvili1@gmail.com>
2025-04-04 16:38:57 +0400

    creating user model with roles

---
+++ src/main/java/com/javaprojects/userauthwithjwt/UserAuthWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.javaprojects.userauthwithjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthWithJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthWithJwtApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/javaprojects/userauthwithjwt/model/Role.java
@@ -0,0 +1,5 @@
+package com.javaprojects.userauthwithjwt.model;
+
+public enum Role {
+    ADMIN, USER
+}
